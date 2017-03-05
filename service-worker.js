2f222b6a147631baab8b664f10f1d902a648ee77
Mathias Ducancel <mathiasduc@gmail.com>
2017-03-05 17:51:38 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","8cd53709ca5ab326143469a919f8a246"],["manifest.json","4765756baa4f156758b765459fc16d8e"],["static/css/main.9f70d834.css","7d1d2a982d83a6780ab1b6a94825a57e"],["static/js/main.fde9d42b.js","b2ae634331cd3642323728b5f5ed4e62"],["static/media/banner_720px.e5d1640d.png","e5d1640d7d63bf5d437c7ee3c4bcc7e7"]];
+var precacheConfig = [["index.html","d75524a1fd7539a03266bb2abcc2a1bb"],["manifest.json","17e0adc7ab2122bbb0916a478a69c3dd"],["static/css/main.9f70d834.css","7d1d2a982d83a6780ab1b6a94825a57e"],["static/js/main.076e6f0a.js","e82cdfbcba458dae64d903722be39fd5"],["static/media/banner_720px.e5d1640d.png","e5d1640d7d63bf5d437c7ee3c4bcc7e7"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
